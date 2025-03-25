336d69f4af670238453a55563505bce0b8d1f2bc
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-25 15:02:42 -0400

    finish day 8

--- src/main/kotlin/solutions/y24/d8/Code.kt
+++ src/main/kotlin/solutions/y24/d8/Code.kt
@@ -3,6 +3,7 @@ package zip.sadan.solutions.y24.d8
 import util.input.UseFile
 import zip.sadan.Solution
 import zip.sadan.util.collections.list.without
+import zip.sadan.util.debug.Solved
 import zip.sadan.util.twoD.Coord
 import zip.sadan.util.twoD.RectangularGrid
 import zip.sadan.util.twoD.times
@@ -13,7 +14,8 @@ class Code : Solution<TInput>() {
     override val year: Number = 24
     override val day: Number = 8
 
-    @UseFile("./test1.txt")
+    @Solved("291")
+    @UseFile("./input.txt")
     override fun part1(input: TInput): Any? {
         val grid = RectangularGrid<Char>(input.map {
             it
@@ -37,7 +39,7 @@ class Code : Solution<TInput>() {
                 for (other in locs without antenna) {
                     val shiftToThis = antenna - other;
                     assert(other + shiftToThis == antenna) { "Logic error, coords should equal" }
-                    antinodes.add(2 * shiftToThis + antenna)
+                    antinodes.add(shiftToThis + antenna)
                 }
             }
         }
@@ -46,7 +48,41 @@ class Code : Solution<TInput>() {
         }
     }
 
+    @Solved("1015")
+    @UseFile("./input.txt")
     override fun part2(input: TInput): Any? {
-        return null
+        val grid = RectangularGrid<Char>(input.map {
+            it
+                .toCharArray()
+                .toList()
+        })
+        val allNodes = input
+            .joinToString("")
+            .toCharArray()
+            .toHashSet()
+        // we only need unique antinodes, overlapping ones dont matter
+        val antinodes = HashSet<Coord>();
+        // not a node, but a blank space
+        allNodes.remove('.');
+        for (nodeChar in allNodes) {
+            val locs = grid.getAll(nodeChar);
+            if (locs.size <= 1) {
+                error("there should always be more than one isntance of a node in the grid");
+            }
+            for (antenna in locs) {
+                for (other in locs without antenna) {
+                    val shiftToThis = antenna - other;
+                    var n = 1;
+                    fun cur() = n * shiftToThis + other
+                    while (cur() in grid) {
+                        antinodes.add(cur())
+                        n++;
+                    }
+                }
+            }
+        }
+        return antinodes.count {
+            it in grid
+        }
     }
 }
